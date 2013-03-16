26dc8e42272e74bb2bbf161d5ffff63d651bfb1c
Ildar Karimov <thevery@yandex-team.ru>
2013-03-17 03:33:22 +0400

    some turn handling

--- src/com/thevery/saboteur/android/MainTest.java
+++ src/com/thevery/saboteur/android/MainTest.java
@@ -1,21 +1,41 @@
 package com.thevery.saboteur.android;
 
-import com.thevery.saboteur.android.model.Card;
-import com.thevery.saboteur.android.model.Game;
-import com.thevery.saboteur.android.model.PathFinishCard;
-import com.thevery.saboteur.android.model.PathStartCard;
+import com.thevery.saboteur.android.model.*;
+import com.thevery.saboteur.android.model.cards.Card;
+import com.thevery.saboteur.android.model.turn.FieldTurn;
+import com.thevery.saboteur.android.model.turn.PlayerTurn;
+import com.thevery.saboteur.android.model.turn.Turn;
 
+import java.util.ArrayList;
 import java.util.List;
 
 public class MainTest {
     public static final int PLAYERS_COUNT = 6;
 
     public static void main(String[] args) {
+        List<Player> players = new ArrayList<Player>(PLAYERS_COUNT);
         Game game = new Game(PLAYERS_COUNT);
+        Field field = game.getField();
         for (int i = 0; i < PLAYERS_COUNT; i++) {
             List<Card> cards = game.getCardsForPlayer(i);
-            System.out.println("cards = " + cards);
+            Player player = new Player();
+            player.setCards(cards);
+            players.add(player);
         }
-//        System.out.println("finishCards = " + finishCards);
+
+        for (int i = 0; i < PLAYERS_COUNT; i++) {
+            Player currentPlayer = players.get(i);
+            Turn turn = currentPlayer.makeTurn();
+            if (turn instanceof FieldTurn) {
+                FieldTurn fieldTurn = (FieldTurn) turn;
+                field.turn(fieldTurn.getCard(), fieldTurn.getX(), fieldTurn.getY());
+            } else if (turn instanceof PlayerTurn) {
+                PlayerTurn playerTurn = (PlayerTurn) turn;
+                Player player = playerTurn.getPlayer();
+                player.takeTurn(playerTurn.getCard());
+            }
+        }
+
+
     }
 }
--- src/com/thevery/saboteur/android/model/Player.java
+++ src/com/thevery/saboteur/android/model/Player.java
@@ -1,10 +1,16 @@
 package com.thevery.saboteur.android.model;
 
+import com.thevery.saboteur.android.model.cards.ActionAbstractPlayerCard;
+import com.thevery.saboteur.android.model.cards.Card;
+import com.thevery.saboteur.android.model.turn.Turn;
+
 import java.util.HashSet;
+import java.util.List;
 import java.util.Set;
 
 public class Player {
     private final Set<Tool> brokenTools = new HashSet<Tool>();
+    private List<Card> cards;
 
     public Player() {
     }
@@ -15,6 +21,18 @@ public class Player {
 
     public boolean repairTool(Tool tool) {
         return brokenTools.remove(tool);
+    }
+
+    public void setCards(List<Card> cards) {
+        this.cards = cards;
+    }
+
+    public Turn makeTurn() {
+        return null;
+    }
+
+    public void takeTurn(ActionAbstractPlayerCard card) {
+        //todo: spy, repair or break
     }
 
     public static enum Tool {
