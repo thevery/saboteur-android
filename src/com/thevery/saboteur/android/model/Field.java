e161df4060bf30c8f7b020886649212b822d1ce8
Ildar Karimov <thevery@yandex-team.ru>
2013-04-04 09:51:46 +0400

    removed cell class

--- src/com/thevery/saboteur/android/model/Field.java
+++ src/com/thevery/saboteur/android/model/Field.java
@@ -8,24 +8,27 @@ import java.util.List;
 public class Field {
     public static final int MINIMAL_DISTANCE = 7;
 
-    private final Array2D<Cell> area = new Array2D<Cell>();
+    private final Array2D<Card> area = new Array2D<Card>();
 
     public Field() {
-        area.put(0, 0, new Cell(PathStartCard.START_CARD));
+        area.put(0, 0, PathStartCard.START_CARD);
         List<Card> finishCards = PathFinishCard.FINISH_CARDS;
-        area.put(MINIMAL_DISTANCE + 1, -1, new Cell(finishCards.get(0)));
-        area.put(MINIMAL_DISTANCE + 1, 0, new Cell(finishCards.get(1)));
-        area.put(MINIMAL_DISTANCE + 1, 1, new Cell(finishCards.get(2)));
+        area.put(MINIMAL_DISTANCE + 1, -1, finishCards.get(0));
+        area.put(MINIMAL_DISTANCE + 1, 0, finishCards.get(1));
+        area.put(MINIMAL_DISTANCE + 1, 1, finishCards.get(2));
     }
 
     public boolean checkTurn(FieldTurnCard card, int x, int y) {
-        Cell cell = area.get(x, y, new Cell());
-        Card currentCard = cell.getCurrentCard();
+        Card currentCard = area.get(x, y);
         if (card instanceof BoomCard) {
             //you cannot boom empty, start or end card
             return currentCard != null && !(currentCard instanceof PathFinishCard) && !(currentCard instanceof PathStartCard);
         } else if (card instanceof PathCard) {
-            //todo: for path check 1) is empty 2) have full path neighbours 3) matches neighbours
+            if (currentCard != null) {
+                return false;//you cannot replace existing card
+            }
+
+            //todo: for path check 2) have full path neighbours 3) matches neighbours
             return false;
         } else {
             throw new IllegalArgumentException("unknown card: " + card);
@@ -38,39 +41,12 @@ public class Field {
             //todo: maybe just return false
             throw new IllegalArgumentException("you cannot make such move!");
         }
-        Cell cell = area.get(x, y, new Cell(card));
         if (card instanceof PathCard) {
-            cell.setCurrentCard(card);
+            area.put(x, y, card);
         } else if (card instanceof BoomCard) {
-            //todo: maybe remove, not null?
-            //todo: maybe make cell = null;
-            cell.setCurrentCard(null);
+            area.put(x, y, null);
         } else {
             throw new IllegalStateException("card must be either path or boom");
-        }
-    }
-
-
-    private static class Cell {
-        private Card currentCard;
-
-        private Cell() {
-        }
-
-        public Cell(Card startCard) {
-            setCurrentCard(startCard);
-        }
-
-        public void removeCurrentCard() {
-            setCurrentCard(null);
-        }
-
-        public Card getCurrentCard() {
-            return currentCard;
-        }
-
-        public void setCurrentCard(Card currentCard) {
-            this.currentCard = currentCard;
         }
     }
 }
